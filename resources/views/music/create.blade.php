8c32e4e371e514041c5df661952326a2197066c2
FixSo-code <qpfh31@gmail.com>
2020-05-03 15:24:34 +0900

    creare music view 프런트엔드

--- resources/views/music/create.blade.php
+++ resources/views/music/create.blade.php
@@ -6,17 +6,68 @@
 
 @section('content')
 
-    <form action="{{ route('music.store') }}" method="post">
-        <input type="text" name="title" placeholder="제목">
-        <input type="text" name="genre" placeholder="장르">
-        <input type="file" name="audio_file">
-        <input type="text" name="youtube_url" placeholder="유튜브 url">
-        <input type="file" name="cover_img_file">
-        <textarea name="description" id="" cols="30" rows="10" placeholder="노래 설명"></textarea>
-        <input type="text" name="composer" placeholder="작곡가">
-        <input type="text" name="editor" placeholder="편곡자">
-        <input type="text" name="lyricist" placeholder="작사가">
-        <input type="text" name="singer" placeholder="보컬">
-    </form>
+    <div class="col-md-7 p-5 mx-auto"
+         style="background-color: #4e555b; margin-top: 100px; height: 1000px; opacity: 0.9; color: #d6d8db">
+
+        <form action="{{ route('music.store') }}" method="post">
+            <div class="form-row">
+                <div class="form-group col-sm-8">
+                    <label for="title">제목</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="제목">
+                </div>
+                <div class="form-group col-sm-4">
+                    <label for="genre">장르</label>
+                    <input type="text" class="form-control" id="genre" name="genre" placeholder="장르">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group pr-2 col-4">
+                    <label for="audio_file">오디오 파일</label>
+                    <input type="file" name="audio_file" id="audio_file"
+                           class="form-control-file btn btn-outline-light btn-sm">
+                </div>
+                <div class="form-group col-8">
+                    <label for="youtube_url">유튜브 Url</label>
+                    <input type="text" class="form-control" name="youtube_url" placeholder="유튜브 Url">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="cover_img_file">커버 이미지</label>
+                <input type="file" name="cover_img_file" id="cover_img_file"
+                       class="form-control-file btn btn-outline-light btn-sm">
+            </div>
+            <div class="form-group">
+                <label for="description">노래 설명</label>
+                <textarea type="text" class="form-control" id="description" name="description"
+                          placeholder="노래 설명" cols="30" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="lyrics">가사</label>
+                <textarea type="text" class="form-control" id="lyrics" name="lyrics"
+                          placeholder="가사" cols="30" rows="10"></textarea>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-sm-3">
+                    <label for="composer">작곡가</label>
+                    <input type="text" class="form-control" id="composer" name="composer" placeholder="작곡가">
+                </div>
+                <div class="form-group col-sm-3">
+                    <label for="editor">편곡자</label>
+                    <input type="text" class="form-control" id="editor" name="editor" placeholder="편곡자">
+                </div>
+                <div class="form-group col-sm-3">
+                    <label for="lyricist">작사가</label>
+                    <input type="text" class="form-control" id="lyricist" name="lyricist" placeholder="작사가">
+                </div>
+                <div class="form-group col-sm-3">
+                    <label for="singer">보컬</label>
+                    <input type="text" class="form-control" id="singer" name="singer" placeholder="보컬">
+                </div>
+            </div>
+            <div class="text-center mt-4">
+                <button type="submit" class="btn btn-outline-light btn-lg">업로드</button>
+            </div>
+        </form>
+    </div>
 
 @endsection
